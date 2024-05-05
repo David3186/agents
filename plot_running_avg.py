e7d5e5beb52c5241ba505b11ac90384e3eabf638
enismaxim1 <enismaxim1@gmail.com>
2024-05-05 19:54:06 -0400

    plotting

--- plot_running_avg.py
+++ plot_running_avg.py
@@ -22,28 +22,27 @@ def get_baseline_reward(game_type):
         rewards.append(total_reward)
     return np.mean(rewards)
 
-def plot_running_avg(output_file, baseline_reward=None):
-    while True:
-        data = [float(x) for x in Path(output_file).read_text().splitlines()]
-        # Plot data as blue and running average of past 50 episodes as red
-        N = len(data)
-        running_avg = np.empty(N)
-        for t in range(N):
-            running_avg[t] = np.mean(data[max(0, t-50):(t+1)])
-        plt.plot(data, label='Episode Lengths', alpha=0.3)
-        if baseline_reward is not None:
-            plt.plot([baseline_reward]*N, label='Baseline', linestyle='--')
-        plt.plot(running_avg, label='Running Average')
-        plt.title("Running Average of Episode Lengths")
-        plt.savefig(f"running_avg_{output_file}.png")
-        time.sleep(15)
-        plt.clf()
+def plot_running_avg(data, output_dir: Path, baseline_reward=None):
+    output_dir = Path(output_dir)
+    # Plot data as blue and running average of past 50 episodes as red
+    N = len(data)
+    running_avg = np.empty(N)
+    for t in range(N):
+        running_avg[t] = np.mean(data[max(0, t-50):(t+1)])
+    plt.plot(data, label='Episode Lengths', alpha=0.3)
+    if baseline_reward is not None:
+        plt.plot([baseline_reward]*N, label='Baseline', linestyle='--')
+    plt.plot(running_avg, label='Running Average')
+    plt.title("Running Average of Episode Lengths")
+    plt.savefig(output_dir / "running_avg.png")
+    plt.clf()
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python plot_running_avg.py <output_file>")
-        sys.exit()
-    output_file = sys.argv[1]
-    baseline = get_baseline_reward("ALE/SpaceInvaders-v5")
-    plot_running_avg(output_file, baseline_reward=baseline)
+    outfile = Path(sys.argv[1])
+    baseline_reward = get_baseline_reward("ALE/SpaceInvaders-v5")
+    while True:
+        data = [float(x) for x in Path(outfile).read_text().strip().split("\n")]
+        output_dir = Path(sys.argv[2])
+        plot_running_avg(data, output_dir, baseline_reward)
+        time.sleep(15)
